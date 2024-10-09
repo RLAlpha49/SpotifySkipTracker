4e1e4b307effb6412e82a7f5dc1a3e68635c5971
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2024-10-09 05:13:08 -0500

    feat ✨: Combined if statements in main playback

--- src/playback.py
+++ src/playback.py
@@ -40,111 +40,111 @@ def main(stop_flag):
 
     while not stop_flag.is_set():
         playback = get_current_playback()
-        if playback:
-            if (
-                playback["is_playing"]
-                and playback["context"]["uri"] == f"spotify:user:{user_id}:collection"
-            ):
-                track_id = playback["item"]["id"]
-                track_name = playback["item"]["name"]
-                # Extract artist names
-                artists = [artist["name"] for artist in playback["item"]["artists"]]
-                artist_names = ", ".join(artists)
-                progress_ms = playback["progress_ms"]
-                duration_ms = playback["item"]["duration_ms"]
+        if (
+            playback
+            and playback["is_playing"]
+            and playback["context"]["uri"] == f"spotify:user:{user_id}:collection"
+        ):
+            track_id = playback["item"]["id"]
+            track_name = playback["item"]["name"]
+            # Extract artist names
+            artists = [artist["name"] for artist in playback["item"]["artists"]]
+            artist_names = ", ".join(artists)
+            progress_ms = playback["progress_ms"]
+            duration_ms = playback["item"]["duration_ms"]
 
-                # Check if it's a new song
-                if track_id != last_track_id:
-                    logger.info(
-                        "New song: %s by %s (%s)", track_name, artist_names, track_id
+            # Check if it's a new song
+            if track_id != last_track_id:
+                logger.info(
+                    "New song: %s by %s (%s)", track_name, artist_names, track_id
+                )
+                # Check if the track is a forward skip
+                if track_id not in track_order:
+                    logger.debug(
+                        "Track not in order: %s by %s (%s)",
+                        track_name,
+                        artist_names,
+                        track_id,
                     )
-                    # Check if the track is a forward skip
-                    if track_id not in track_order:
+                    # Fetch recently played tracks
+                    recently_played_data = get_recently_played_tracks()
+                    recently_played = recently_played_data.get("tracks", [])
+
+                    # Track is not in the order and not recently played, it's a forward skip
+                    if track_id not in recently_played:
                         logger.debug(
-                            "Track not in order: %s by %s (%s)",
+                            "Track not in recently played: %s by %s (%s)",
                             track_name,
                             artist_names,
                             track_id,
                         )
-                        # Fetch recently played tracks
-                        recently_played_data = get_recently_played_tracks()
-                        recently_played = recently_played_data.get("tracks", [])
-
-                        # Track is not in the order and not recently played, it's a forward skip
-                        if track_id not in recently_played:
+                        if check_if_skipped_early(last_progress, last_duration_ms):
                             logger.debug(
-                                "Track not in recently played: %s by %s (%s)",
+                                "Track is a skipped early: %s by %s (%s)",
                                 track_name,
                                 artist_names,
                                 track_id,
                             )
-                            if check_if_skipped_early(last_progress, last_duration_ms):
-                                logger.debug(
-                                    "Track is a skipped early: %s by %s (%s)",
-                                    track_name,
-                                    artist_names,
-                                    track_id,
-                                )
-                                if last_track_id in skip_count:
-                                    skip_count[last_track_id] += 1
-                                else:
-                                    skip_count[last_track_id] = 1
+                            if last_track_id in skip_count:
+                                skip_count[last_track_id] += 1
+                            else:
+                                skip_count[last_track_id] = 1
 
+                            logger.info(
+                                "Song '%s' by %s (%s) skipped %d times.",
+                                last_track_name,
+                                last_artist_names,
+                                last_track_id,
+                                skip_count[last_track_id],
+                            )
+
+                            # Unlike if skipped 5 times
+                            if skip_count[last_track_id] >= 5:
                                 logger.info(
-                                    "Song '%s' by %s (%s) skipped %d times.",
+                                    "Unliking song: %s by %s (%s)",
                                     last_track_name,
                                     last_artist_names,
                                     last_track_id,
-                                    skip_count[last_track_id],
                                 )
+                                unlike_song(last_track_id)
+                                skip_count.pop(last_track_id)
 
-                                # Unlike if skipped 5 times
-                                if skip_count[last_track_id] >= 5:
-                                    logger.info(
-                                        "Unliking song: %s by %s (%s)",
-                                        last_track_name,
-                                        last_artist_names,
-                                        last_track_id,
-                                    )
-                                    unlike_song(last_track_id)
-                                    skip_count.pop(last_track_id)
-
-                                save_skip_count(skip_count)
-                            else:
-                                logger.debug(
-                                    "Track is not skipped early: %s by %s (%s)",
-                                    track_name,
-                                    artist_names,
-                                    track_id,
-                                )
+                            save_skip_count(skip_count)
                         else:
                             logger.debug(
-                                "Track in recently played: %s by %s (%s)",
+                                "Track is not skipped early: %s by %s (%s)",
                                 track_name,
                                 artist_names,
                                 track_id,
                             )
                     else:
                         logger.debug(
-                            "Track in order: %s by %s (%s)",
+                            "Track in recently played: %s by %s (%s)",
                             track_name,
                             artist_names,
                             track_id,
                         )
+                else:
+                    logger.debug(
+                        "Track in order: %s by %s (%s)",
+                        track_name,
+                        artist_names,
+                        track_id,
+                    )
 
-                    # Update track order
-                    track_order.append(track_id)
-                    if len(track_order) > 1000:
-                        logger.info("Track order limit reached: %s", track_order)
-                        track_order.pop(0)
+                # Update track order
+                track_order.append(track_id)
+                if len(track_order) > 1000:
+                    logger.info("Track order limit reached: %s", track_order)
+                    track_order.pop(0)
 
-                    # Update last track details
-                    last_track_id = track_id
-                    last_track_name = track_name
-                    last_artist_names = artist_names
-                    last_duration_ms = duration_ms
+                # Update last track details
+                last_track_id = track_id
+                last_track_name = track_name
+                last_artist_names = artist_names
+                last_duration_ms = duration_ms
 
-                last_progress = progress_ms
+            last_progress = progress_ms
 
         time.sleep(0.5)
     logger.info("Playback monitoring stopped.")
